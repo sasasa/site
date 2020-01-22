9809f50c13e94f917438e2aeb978da0ec3b27af8
sasasa <masaakisaeki@gmail.com>
2020-01-22 16:39:18 +0900

    管理画面の仕様が分かりにくい為修正する #25
    入力方法と文言を変更

--- src/input.js
+++ src/input.js
@@ -5,6 +5,7 @@ import moment from 'moment'
 import "bootstrap";
 import { escape_html, BOX_ID, JBN_ID } from './module/util'
 
+import "./lib/bootstrap-datetimepicker.min.js"
 import './input.scss'
 
 
@@ -94,3 +95,25 @@ $('#event_btn').click(async () => {
 
 
 
+$('.datetimepicker').datetimepicker({
+  dayViewHeaderFormat: 'YYYY年 MMMM',
+  tooltips: {
+      close: '閉じる',
+      selectMonth: '月を選択',
+      prevMonth: '前月',
+      nextMonth: '次月',
+      selectYear: '年を選択',
+      prevYear: '前年',
+      nextYear: '次年',
+      selectTime: '時間を選択',
+      selectDate: '日付を選択',
+      prevDecade: '前期間',
+      nextDecade: '次期間',
+      selectDecade: '期間を選択',
+      prevCentury: '前世紀',
+      nextCentury: '次世紀'
+  },
+  format: 'YYYY.MM.DD',
+  locale: 'ja',
+  showClose: true
+});
